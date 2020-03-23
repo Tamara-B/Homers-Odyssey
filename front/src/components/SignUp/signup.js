328d3d25867f5b39b25b1cb14c782dc68daa6f46
Tamara Bonyai <tamamara18@gmail.com>
2020-03-23 15:22:48 +0100

    Quest 020 Basic registration form

--- front/src/components/SignUp/signup.js
+++ front/src/components/SignUp/signup.js
@@ -5,20 +5,39 @@ class SignUp extends Component {
     super(props);
     this.state = {
       email: "",
+      password: "",
+      passwordConf: "",
+      firstname: "",
+      lastname: "",
     };
   }
-  updateEmailField(event) {
-    this.setState({
-      email: event.target.value
-    })
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state, 1, 1));
+    if (this.state.password !== this.state.passwordConf) {
+      alert('Passwords aren\'t the same!');
+    } else {
+      alert('Confirmation e-mail sent to: ' + this.state.email)
+    }
   }
 
   render() {
     return (
-      <div>
-        <h1>E-mail address:{this.state.email}</h1>
-        <input type="email" name="email" placeholder="name@email.hu" onChange={this.updateEmailField.bind(this)} />
-      </div>
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <input type="email" name="email" placeholder="name@email.hu" value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} />
+        <br />
+        <input type="password" name="password" value={this.state.password} placeholder="password" onChange={(event) => this.setState({ password: event.target.value })}></input>
+        <br />
+        <input type="password" name="passwordConf" value={this.state.passwordConf} placeholder="Confirm password" onChange={(event) => this.setState({ passwordConf: event.target.value })}></input>
+        <br />
+        <input type="text" name="firstname" value={this.state.firstname} placeholder="First name" onChange={(event) => this.setState({ firstname: event.target.value })}></input>
+        <br />
+        <input type="text" name="lastname" value={this.state.lastname} placeholder="Last name" onChange={(event) => this.setState({ lastname: event.target.value })}></input>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
