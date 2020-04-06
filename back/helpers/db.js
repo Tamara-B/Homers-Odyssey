4ebb42438756c412df8c1b3ef8ac23261074885d
Tama <39649956+Tamara-B@users.noreply.github.com>
2020-04-06 11:13:44 +0200

    Delete db.js

--- back/helpers/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Timtom18!',
-  database: 'wild_db_quest'
-});
-module.exports = connection;
